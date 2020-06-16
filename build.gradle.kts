5b08665a22c9b5a943471e48a0f6c26b1fb671d1
Malox10 <Maxi.Junges@gmx.de>
2020-06-16 16:36:11 +0200

    add database dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ subprojects {
 
 	repositories {
 		mavenCentral()
+		jcenter()
+		maven("https://dl.bintray.com/kotlin/kotlin-eap")
 		setOf(
 			"kotlin-eap",
 			"ktor",
@@ -41,6 +43,13 @@ subprojects {
 project(":server") {
 	dependencies {
 		implementation(project(":common"))
+		setOf(
+			"core",
+			"dao",
+			"jdbc"
+		).forEach { implementation("org.jetbrains.exposed", "exposed-$it", "0.24.1") }
+		implementation("org.xerial","sqlite-jdbc","3.30.1")
+		implementation("org.slf4j","slf4j-simple","1.7.25")
 	}
 }
 
