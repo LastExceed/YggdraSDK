41c10fc177683a05d4e7041085fd30ab07cc37e9
Malox10 <Maxi.Junges@gmx.de>
2020-06-17 19:18:20 +0200

    make name unique

--- server/src/main/kotlin/database/tables/TableUser.kt
+++ server/src/main/kotlin/database/tables/TableUser.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Table
 
 object TableUser : Table() {
 	val id = long("id").autoIncrement().uniqueIndex()
-	val name = varchar("name", 24) //hardcoded
+	val name = varchar("name", 24).uniqueIndex() //hardcoded
 
 	override val primaryKey = PrimaryKey(id)
 }
