859f8866cc5c538dfd1751286d03ce51a7c0a204
Malox10 <Maxi.Junges@gmx.de>
2020-08-03 16:34:02 +0200

    reimplement Packet abstract class

--- common/src/main/kotlin/packet/Packet.kt
+++ common/src/main/kotlin/packet/Packet.kt
@@ -1,11 +1,14 @@
 package packet
 
+import io.ktor.utils.io.*
+
 abstract class Packet(val id: PacketId) {
-//	abstract suspend fun deserialize(readChannel: ByteReadChannel) TODO resolve this
-//	suspend fun serialize(writeChannel: ByteWriteChannel) {
-//		writeChannel.writeByte(id.value)
-//		writePacketContent(writeChannel)
-//	}
-//
-//	protected abstract suspend fun writePacketContent(writeChannel: ByteWriteChannel)
+	protected abstract suspend fun writePacketContent(writer: ByteWriteChannel)
+
+	companion object {
+		suspend fun ByteWriteChannel.writePacket(packet: Packet) {
+			this.writeByte(packet.id.value)
+			packet.writePacketContent(this)
+		}
+	}
 }
