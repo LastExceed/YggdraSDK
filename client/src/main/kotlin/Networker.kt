46dfe5ae5e5fa2084c37bddcb7b1c5aa12e545bf
Malox10 <Maxi.Junges@gmx.de>
2020-08-16 01:15:29 +0200

    add stop to Networker

--- client/src/main/kotlin/Networker.kt
+++ client/src/main/kotlin/Networker.kt
@@ -1,7 +1,9 @@
 import frontend.components.NodeCached
 import io.ktor.network.sockets.*
 import io.ktor.utils.io.*
+import javafx.application.Application.launch
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import packet.*
 import packet.Packet.Companion.writePacket
@@ -11,6 +13,7 @@ class Networker(val address: InetSocketAddress) {
 	private lateinit var socket: Socket
 	private lateinit var reader: ByteReadChannel
 	private lateinit var writer: ByteWriteChannel
+	private var isRunning: Boolean = true
 
 	suspend fun connect(email: String, password: String): Boolean {
 		socket = Globals.tcpSocketBuilder.connect(address)
@@ -27,15 +30,28 @@ class Networker(val address: InetSocketAddress) {
 
 	suspend fun handlePackets() {
 		writer.writePacket(PacketGoTo(NodeId(1L)))//TODO: dont hardcode root ID
-		while (true) {
-			val packetID = PacketId(reader.readByte())
-			when (packetID) { //use a map instead
-				PacketId.NODE_REVEAL -> onPacketNodeReveal()
+		while (isRunning) {
+			try {
+				val packetID = PacketId(reader.readByte())
+				when (packetID) { //use a map instead
+					PacketId.NODE_REVEAL -> onPacketNodeReveal()
 
-				else -> error("unknown packetId: $packetID")
+					else -> error("unknown packetId: $packetID")
+				}
+			} catch (e: Throwable) {
+				println("Socket closed with message: ${e.message}")
 			}
 		}
 	}
+
+	fun stop() {
+		try {
+			isRunning = false
+			socket.dispose()
+		} catch (e: Throwable) {
+			println(e.message)
+		}
+	} //TODO send disconnect package?
 
 	private suspend fun login(email: String, password: String): Boolean {
 		writer.writePacket(PacketLogin(email, password))
