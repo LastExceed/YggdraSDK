e63b71d4fc145b49e2838796ddc2e08bc0bd4c1c
Malox10 <Maxi.Junges@gmx.de>
2020-08-15 14:05:16 +0200

    make passwords not unique

--- server/src/main/kotlin/database/tables/TableUser.kt
+++ server/src/main/kotlin/database/tables/TableUser.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.Table
 object TableUser : Table() {
 	val id = long("id").autoIncrement().uniqueIndex()
 	val email = varchar("email", Globals.emailSizeLimit).uniqueIndex()
-	val password = varchar("password", Globals.passwordSizeLimit).uniqueIndex()
+	val password = varchar("password", Globals.passwordSizeLimit)
 
 	override val primaryKey = PrimaryKey(id)
 }
