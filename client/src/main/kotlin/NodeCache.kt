71c42932d8a499278a633ef49f30f2bc80bc60c9
Malox10 <Maxi.Junges@gmx.de>
2020-08-16 21:20:50 +0200

    add Todo

--- client/src/main/kotlin/NodeCache.kt
+++ client/src/main/kotlin/NodeCache.kt
@@ -1,6 +1,7 @@
 import frontend.components.NodeCached
 
 //TODO: request root from server
+//TODO: make instantiable for testing
 object NodeCache {
 	val root = NodeCached(NodeId(1), false, "root of all evil", null)
 
